09dfc98666a296035d134ce1d4e79366b229236b
manikrish66 <manikrish270@gmail.com>
2023-04-27 12:46:50 -0700

    Hello

---
+++ src/org/inm/InternDetail.java
@@ -0,0 +1,14 @@
+package org.inm;
+
+public class InternDetail {
+	private void internname() {
+	System.out.println(" Mani " );
+  }
+	private void registeredinternship() {
+	System.out.println(" Inmakes ");
+  }
+    private void internshipduration() {
+    System.out.println(" 3 Months ");
+  }
+    
+}
---
+++ src/org/inm/InternEducation.java
@@ -0,0 +1,16 @@
+package org.inm;
+
+public class InternEducation {
+	private void ugdegree() {
+	System.out.println(" B.SC ");
+   }
+    private void ugpercentage() {
+	System.out.println(" 88 ");
+   }
+    public static void main(String[] args) {
+	//ClassName object = new ClassName();
+     InternEducation p = new InternEducation();
+     p.ugdegree();
+     p.ugpercentage();
+	}
+}
